b48556f15dc29da6d16eb802c2340525be814dbe
oder_1 <110951020+Code-by-Ashish-Thakur@users.noreply.github.com>
2024-02-17 16:13:29 +0530

    Update country-codes.sql

--- country-codes.sql
+++ country-codes.sql
@@ -1,3 +1,10 @@
+#Github- https://github.com/Code-by-Ashish-Thakur
+#Leetcode- https://leetcode.com/Ashish_49
+#Codeforces- https://codeforces.com/profile/oder_1
+#Codechef- https://www.codechef.com/users/oder_1
+
+# MY MySQL query statement below
+
 SELECT a.customer_id,a.name,concat("+",b.country_code,a.phone_number)
 FROM customers as a
 LEFT join country_codes as b 
