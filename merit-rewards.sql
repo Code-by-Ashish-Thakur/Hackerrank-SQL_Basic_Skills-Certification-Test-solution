5700a2ff88b013e26fd4f77623c6a112b56cacca
oder_1 <110951020+Code-by-Ashish-Thakur@users.noreply.github.com>
2024-02-17 16:13:54 +0530

    Update merit-rewards.sql

--- merit-rewards.sql
+++ merit-rewards.sql
@@ -1,3 +1,10 @@
+#Github- https://github.com/Code-by-Ashish-Thakur
+#Leetcode- https://leetcode.com/Ashish_49
+#Codeforces- https://codeforces.com/profile/oder_1
+#Codechef- https://www.codechef.com/users/oder_1
+
+# MY MySQL query statement below
+
 SELECT ei.employee_ID, ei.name
 FROM employee_information ei
 JOIN last_quarter_bonus b ON b.employee_ID = ei.employee_ID
