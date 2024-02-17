03b93dd3edc47898e3354e2f1298e8e6f45bd318
oder_1 <110951020+Code-by-Ashish-Thakur@users.noreply.github.com>
2024-02-17 16:14:53 +0530

    Update student-advisor.sql

--- student-advisor.sql
+++ student-advisor.sql
@@ -1,3 +1,10 @@
+#Github- https://github.com/Code-by-Ashish-Thakur
+#Leetcode- https://leetcode.com/Ashish_49
+#Codeforces- https://codeforces.com/profile/oder_1
+#Codechef- https://www.codechef.com/users/oder_1
+
+# MY MySQL query statement below
+
 SELECT roll_number,name
 FROM student_information a
 INNER JOIN faculty_information b
